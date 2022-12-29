ca143c7ce2faec0a4d7e9a32924d322b80e67998
Valerya <blerao@icloud.com>
2022-12-29 17:10:32 +0300

    8

---
+++ HW1_8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while (true)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine (i + " ");   
+    }
+    if (i >= N)
+    {
+        break;
+    }
+    i++;
+}
