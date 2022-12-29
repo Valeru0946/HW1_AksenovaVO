b73ab0ad0ffead1c68c124b050b1acccdecc609c
Valerya <blerao@icloud.com>
2022-12-29 17:09:51 +0300

    6

---
+++ HW1_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+     Console.WriteLine("нечетное число");
+}
