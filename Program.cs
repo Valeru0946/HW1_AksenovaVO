9ded04ce1a8d98a27a7c2d9af63ace7af0a37ec0
Valerya <blerao@icloud.com>
2022-12-29 17:11:15 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine ());
+if (a > b)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+if (a == b)
+{
+     Console.WriteLine("Числа равны");
+}
+if (a < b)
+{
+     Console.WriteLine("Второе число больше первого");
+}
+    
