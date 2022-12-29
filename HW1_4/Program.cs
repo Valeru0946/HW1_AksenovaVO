a2f5c7ba2e4a5d0d5c44b5e474498e7da159db31
Valerya <blerao@icloud.com>
2022-12-29 17:08:46 +0300

    4

---
+++ HW1_4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 4
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите третье число");
+int d = Convert.ToInt32(Console.ReadLine ());
+if (a < b)
+{
+    if (d > b)
+    {
+        Console.WriteLine("Третье число максимальное");
+    }
+}
+if (a > b)
+{
+    if (a > d)
+    {
+        Console.WriteLine("Первое число максимальное");
+    }
+}
+if (b > a)
+{
+     if (b > d)
+    {
+        Console.WriteLine("Второе число максимальное");
+    }
+}
+if (a ==b)
+{
+     if (b == d)
+    {
+        Console.WriteLine("Числа равны");
+    }
+}
+
